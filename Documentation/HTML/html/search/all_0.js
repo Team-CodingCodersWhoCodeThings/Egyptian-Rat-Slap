2faa3661ccbf6dc3e60823b0e2531e8fd40d8063
Joey Pennington <jpennington222@ku.edu>
2019-11-03 19:12:36 -0600

    Add HTML documentation

---
+++ Documentation/HTML/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['change_5fscene_0',['Change_Scene',['../class_change___scene.html',1,'']]],
+  ['changemenuscene_1',['changemenuscene',['../class_change___scene.html#a948f335c75f84eb741030f2f881a1549',1,'Change_Scene']]]
+];
---
+++ Documentation/HTML/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dealcards_2',['dealCards',['../class_e_r_s.html#a99901a1421a484123ebd77e2452baa84',1,'ERS']]]
+];
