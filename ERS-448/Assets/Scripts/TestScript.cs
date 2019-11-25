61ec46a48ea133d259cd88fe61691527addb6743
allemanz <49819742+allemanz@users.noreply.github.com>
2019-11-25 13:54:31 -0600

    Added test for startGame

--- ERS-448/Assets/Scripts/TestScript.cs
+++ ERS-448/Assets/Scripts/TestScript.cs
@@ -5,9 +5,39 @@ using IngameDebugConsole;
 
 public class TestScript : MonoBehaviour
 {
+    private static ERS ers;
+
+    void Start()
+    {
+        
+    }
+
     [ConsoleMethod( "test", "Runs test of game functions" )]
-	public static void RunTest()
+	public static void ConsoleTest()
 	{
-		print("test");
+        ers = FindObjectOfType<ERS>();
+        bool test = true;
+        for(int i = 0; i < 26; i++)
+        {
+            if(ers.AIDeck[i] != ers.deck[i * 2])
+            {
+                test = false;
+            }
+        }
+        for(int i = 0; i < 26; i++)
+        {
+            if(ers.PlayerDeck[i] != ers.deck[(i * 2) + 1])
+            {
+                test = false;
+            }
+        }
+        if(test)
+        {
+            print("Test 1: startGame evenly splits 52 cards between players: SUCCESS");
+        }
+        else
+        {
+            print("Test 1: startGame evenly splits 52 cards between players: FAILURE");
+        }
 	}
 }
